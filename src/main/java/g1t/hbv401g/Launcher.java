41da1ee3f87211f357f868911f8d5f5e4849a81d
Viktor Andri <viktorandri17@gmail.com>
2026-03-04 20:06:50 +0000

    init

---
+++ src/main/java/g1t/hbv401g/Launcher.java
@@ -0,0 +1,9 @@
+package g1t.hbv401g;
+
+import javafx.application.Application;
+
+public class Launcher {
+    public static void main(String[] args) {
+        Application.launch(HelloApplication.class, args);
+    }
+}
---
+++ src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module g1t.hbv401g {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+
+    opens g1t.hbv401g to javafx.fxml;
+    exports g1t.hbv401g;
+}
